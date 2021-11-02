3ee7a05addceedf03ec5c394783433773959e3c2
Birat Ale <biratmrg@gmail.com>
2021-11-02 15:23:16 +0000

    THHHHHE GIHUN

--- fundamentals of programming/console game/Program.cs
+++ fundamentals of programming/console game/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Threading;
 
 namespace console_game
 {
     class Program
     {
+
+        static void Welcome()
+        {
+            Console.WriteLine("Welcome");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Welcome();
         }
     }
 }
