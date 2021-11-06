550adf9c8782586c2c4cedc13cf81968f06de71d
Birat Ale <biratmrg@gmail.com>
2021-11-06 17:57:03 +0000

    making board

--- fundamentals of programming/console game/Program.cs
+++ fundamentals of programming/console game/Program.cs
@@ -8,25 +8,36 @@ namespace console_game
 
         static void Welcome()
         {
-            Console.WriteLine(@"          _______  _        _______  _______  _______  _______ 
-|\     /|(  ____ \( \      (  ____ \(  ___  )(       )(  ____ \
-| )   ( || (    \/| (      | (    \/| (   ) || () () || (    \/
-| | _ | || (__    | |      | |      | |   | || || || || (__    
-| |( )| ||  __)   | |      | |      | |   | || |(_)| ||  __)   
-| || || || (      | |      | |      | |   | || |   | || (      
-| () () || (____/\| (____/\| (____/\| (___) || )   ( || (____/\
-(_______)(_______/(_______/(_______/(_______)|/     \|(_______/
-                                                               ");
+            Console.WriteLine("Welcome to Tic-tac-toe!");
             Thread.Sleep(2000);
             Console.Clear();
         }
 
 
+        static void generateBoard()
+        {
+            int row = 5;
+            int col = 5;
+
+            string[,] board = new string[row, col];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int v = 0; v < 5; v++)
+                {
+                    board[i, v] = " 1";
+                    Console.Write(board[i, v]);
+                }
+
+            }
+        }
+
         static void Main(string[] args)
         {
             Welcome();
 
-            string[,] array = new string[,];
+            generateBoard();
+            
 
         }
     }
